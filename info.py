7dfd79bf864d53e36488f158772f3f663f1d6935
Aadil0077 <160581969+Aadil0077@users.noreply.github.com>
2025-01-25 01:27:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "electoral-allyn-moviehubl-d540d45c.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
